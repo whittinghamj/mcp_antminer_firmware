4877be0ca8ee95acd683b4aa0023f67c4ef0e5b5
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-16 20:39:19 +0100

    no message

--- push_to_miners.sh
+++ push_to_miners.sh
@@ -2,5 +2,10 @@
 
 ## MCP Antminer Firmware - DEV SCP Script
 
+echo 'Pushing index.html to remote miners for dev.'
+
 sshpass -p 'admin' scp index.html root@192.168.7.61:/www/pages/
 sshpass -p 'admin' scp index.html root@192.168.7.66:/www/pages/
+
+echo 'Done'
+echo ''
