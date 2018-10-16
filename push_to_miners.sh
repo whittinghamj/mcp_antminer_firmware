166f48e83406dc5dd6cbf9e077eb1578a2adf27f
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-16 20:34:33 +0100

    Added push script to update local miners with updated web UI files.

---
+++ push_to_miners.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## MCP Antminer Firmware - DEV SCP Script
+
+sshpass -p 'admin' scp index.html root@192.168.7.61:/www/pages/
+sshpass -p 'admin' scp index.html root@192.168.7.66:/www/pages/
